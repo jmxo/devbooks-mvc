d136070c54fc4ce1f52d14af3e3312cbdff4862d
mohamed-ismat <mnb1236@live.com>
2017-03-02 22:42:05 +0300

    added web project,, modified default views

---
+++ DevBooks.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevBooks.Web.Startup))]
+namespace DevBooks.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
