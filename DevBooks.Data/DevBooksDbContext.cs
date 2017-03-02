6c7c9be74dfe122c5440f45181bf9087c605415e
mohamed-ismat <mnb1236@live.com>
2017-03-02 07:56:59 +0300

    added basic models and DbContext

---
+++ DevBooks.Data/DevBooksDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevBooks.Models;
+using System.Data.Entity;
+using System.Configuration;
+
+namespace DevBooks.Data
+{
+    public class DevBooksDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public static string ConnectionStringName
+        {
+            get
+            {
+                if (ConfigurationManager.AppSettings["ConnectionStringName"] != null)
+                {
+                    return ConfigurationManager.AppSettings["ConnectionStringName"].ToString();
+                }
+
+                return "DefaultConnection";
+            }
+        }
+
+        public DevBooksDbContext() : base(nameOrConnectionString: DevBooksDbContext.ConnectionStringName) { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            
+            //base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ DevBooks.Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBooks.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Isbn { get; set; }
+        public int? Price { get; set; }
+        public int? Pages { get; set; }
+        public string  Description { get; set; }
+        public string Publisher { get; set; }
+        public string ImageName { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+    }
+}
