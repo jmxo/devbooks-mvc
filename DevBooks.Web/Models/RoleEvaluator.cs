f4fe3120cca6080d1926d92e45ec62e583dee719
mohamed-ismat <mnb1236@live.com>
2017-03-06 18:55:26 +0300

    Implemented the RoleEvaluator class and the base VM

---
+++ DevBooks.Web/Models/RoleEvaluator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace DevBooks.Web.Models
+{
+    public class RoleEvaluator
+    {
+        public bool CanEdit
+        {
+            get
+            {
+                return Roles.IsUserInRole("Admin") ||
+                        Roles.IsUserInRole("Manager") ||
+                        Roles.IsUserInRole("User");
+            }
+        }
+
+        public bool CanDelete
+        {
+            get
+            {
+                return Roles.IsUserInRole("Admin") ||
+                        Roles.IsUserInRole("Manager");
+            }
+        }
+    }
+}
