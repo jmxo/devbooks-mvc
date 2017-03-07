f00b060a41e8b71524191463c2292bf0307ee30c
mohamed-ismat <mnb1236@live.com>
2017-03-07 20:20:21 +0300

    Inheriting from ViewModelBase

---
+++ DevBooks.Web/ViewModels/BookViewModel.cs
@@ -0,0 +1,24 @@
+﻿using DevBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DevBooks.Web.ViewModels
+{
+    public class BookViewModel : ViewModelBase
+    {
+        public Book Book { get; set; }
+        public bool IsNew { get; set; }
+
+        public string ImageUrlPrefix
+        {
+            get { return DevBooks.Web.Config.ImagesUrlPrefix; }
+        }
+
+        public BookViewModel()
+        {
+            this.Book = new Book();
+        }
+    }
+}
