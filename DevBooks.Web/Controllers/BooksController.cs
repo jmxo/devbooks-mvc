485064376274f964c7ecdc95b63dd8f7d868d2e9
mohamed-ismat <mnb1236@live.com>
2017-03-07 20:40:51 +0300

    Implemented Edit actions in BooksController

--- DevBooks.Web/Controllers/BooksController.cs
+++ DevBooks.Web/Controllers/BooksController.cs
@@ -1,7 +1,9 @@
 ﻿using DevBooks.Data;
+using DevBooks.Models;
 using DevBooks.Web.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -20,6 +22,89 @@ namespace DevBooks.Web.Controllers
             var query = _unit.Books.GetAll().OrderBy(b => b.Title);
             vm.Books = query.ToList();
             return View(vm);
+        }
+
+        public ActionResult New()
+        {
+            BookViewModel vm = new BookViewModel();
+            vm.IsNew = true;
+
+            return View("Book", vm);
+        }
+
+        [ActionName("Edit")]
+        public ActionResult Get(int id)
+        {
+            BookViewModel vm = new BookViewModel();
+
+            vm.Book = this._unit.Books.GetById(id);
+
+            if (vm.Book != null)
+            {
+                return View("Book", vm);
+            }
+
+            return View("NotFound");
+        }
+
+        [HttpPost()]
+        public ActionResult UploadImage(HttpPostedFileBase image, int id)
+        {
+            JsonResult result;
+            Book book;
+            Random rand = new Random();
+            string unique;
+            string ext;
+            string fileName;
+            string path;
+
+            unique = rand.Next(1000000).ToString();
+
+            ext = Path.GetExtension(image.FileName).ToLower();
+
+            fileName = string.Format("{0}-{1}{2}", id, unique, ext);
+
+            path = Path.Combine(HttpContext.Server.MapPath(Config.ImagesFolderPath), fileName);
+
+            if (ext == ".png" || ext == ".jpg")
+            {
+                book = _unit.Books.GetById(id);
+
+                if (book != null)
+                {
+                    book.ImageName = fileName;
+                    _unit.Books.Update(book);
+                    _unit.SaveChanges();
+
+                    image.SaveAs(path);
+                    result = this.Json(new
+                    {
+                        imageUrl = string.Format("{0}{1}",
+                        Config.ImagesUrlPrefix, fileName)
+                    });
+                }
+                else
+                {
+                    result = this.Json(new
+                    {
+                        status = "error",
+                        statusText =
+                            string.Format("There is no book with the Id of " +
+                                "'{0}' in the system.", id)
+                    });
+                }
+            }
+            else
+            {
+                result = this.Json(new
+                {
+                    status = "error",
+                    statusText = "Unsupported image type. Only .png or " +
+                        ".jpg files are acceptable."
+                });
+            }
+
+            return result;
         }
 
         protected override void Dispose(bool disposing)
